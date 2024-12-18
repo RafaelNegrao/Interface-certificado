7fc018667aa4b5ab4377b0574a825c38cc57269c
Rafael Negrão <rafaelneg2015@gmail.com>
2024-12-17 21:39:28 -0300

    Corrigida criação de pasta

--- source.py
+++ source.py
@@ -13,6 +13,11 @@ import sys
 import subprocess
 import math
 import pyperclip
+import numpy as np
+import mplcursors
+import calendar  
+import smtplib
+import matplotlib.pyplot as plt
 from tkinter import filedialog
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
@@ -41,23 +46,17 @@ QLabel,
 QHBoxLayout
 )
 from PyQt5.QtCore import QDate, QTime,QUrl, Qt,QTimer,QRect,QRegExp, QDateTime
-from PyQt5.QtGui import QDesktopServices,QColor,QRegExpValidator, QPalette, QBrush, QLinearGradient
+from PyQt5.QtGui import QDesktopServices,QColor,QRegExpValidator
 from Interface import Ui_janela
 from firebase_admin import db
-from requests.exceptions import RequestException
 from credenciaisBd import *
-import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-import json
-import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from collections import Counter,defaultdict
-import numpy as np
-import mplcursors
-import calendar  
+from collections import defaultdict
 from interfaceUpdates import AlteracoesInterface
 from update import Atualizar
+from login import LoginWindow
 
 
 
@@ -71,6 +70,7 @@ class FuncoesPadrao:
         self.ui = ui
         self.parent = parent
         self.dicionario = None
+        self.acoes = AcoesBancoDeDados
 
 
     def evento_ao_abrir(self,event):
@@ -667,7 +667,7 @@ class FuncoesPadrao:
                 else:
                     AlteracoesInterface.confirmar_label_criar_pasta(self)
 
-                self.acoes.salvar_pedido()
+                AcoesBancoDeDados.salvar_pedido(self)
             #Caso exista, abra
             else:
                 self.abrir_pasta_cliente()
@@ -2783,136 +2783,6 @@ Vendas..........{venda}
 
 
 
-class LoginWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-      
-        self.fundo_cor = "rgb(40, 42, 54)" 
-        self.campo_texto_cor = "rgb(210, 210, 210)"  
-        self.campo_fundo_cor = "rgb(30, 30, 30)" 
-        self.botao_cor = "rgb(0, 122, 255)"  
-        self.cor_login = "rgb(210, 210, 210)"
-
-        self.setWindowTitle("Login")
-        self.setFixedSize(300, 300)
-
-      
-        self.central_widget = QWidget()
-        self.setCentralWidget(self.central_widget)
-
-       
-        layout = QVBoxLayout()
-        layout.setAlignment(Qt.AlignCenter)
-        layout.setSpacing(20)  
-
-       
-        self.label_titulo = QLabel("Login")
-        self.label_titulo.setStyleSheet(f"font-size: 24px; font-weight: bold; color: {self.cor_login}; font-family: Consolas;")
-        self.label_titulo.setAlignment(Qt.AlignCenter)
-        layout.addWidget(self.label_titulo)
-
-        
-        self.campo_usuario = QLineEdit()
-        self.campo_usuario.setPlaceholderText("Nome")
-        self.campo_usuario.setStyleSheet(f"font-size: 18px; height: 35px; border-radius: 5px; padding: 5px; background-color: {self.campo_fundo_cor}; color: {self.campo_texto_cor}; border: 1px solid {self.campo_texto_cor}; font-family: Consolas;")
-        self.campo_usuario.setFocusPolicy(Qt.StrongFocus)  
-        layout.addWidget(self.campo_usuario)
-
-    
-        senha_layout = QHBoxLayout()
-        senha_layout.setContentsMargins(0, 0, 0, 0) 
-        self.campo_senha = QLineEdit()
-        self.campo_senha.setPlaceholderText("Senha")
-        self.campo_senha.setEchoMode(QLineEdit.Password)
-        self.campo_senha.setStyleSheet(f"font-size: 18px; height: 35px; border-radius: 5px; padding: 5px; background-color: {self.campo_fundo_cor}; color: {self.campo_texto_cor}; border: 1px solid {self.campo_texto_cor}; font-family: Consolas;")
-        self.campo_senha.setFocusPolicy(Qt.StrongFocus)  
-        senha_layout.addWidget(self.campo_senha)
-
-
-        self.botao_olho = QPushButton("👁️")
-        self.botao_olho.setStyleSheet(f"font-size: 25px; height: 40px; background-color: transparent; color: {self.campo_texto_cor}; border: none; padding: 0; font-family: Consolas;")
-        self.botao_olho.setFixedSize(35, 35)  
-        self.botao_olho.clicked.connect(self.toggle_senha_visivel)
-        senha_layout.addWidget(self.botao_olho)
-
-        layout.addLayout(senha_layout)
-
-        
-        self.botao_login = QPushButton("Entrar")
-        self.botao_login.setStyleSheet(f"font-size: 16px; height: 40px; background-color: {self.botao_cor}; color: {self.campo_texto_cor}; border-radius: 5px; font-family: Consolas;")
-        self.botao_login.clicked.connect(self.fazer_login)
-        layout.addWidget(self.botao_login)
-
-        
-        self.label_mensagem = QLabel("")
-        self.label_mensagem.setStyleSheet(f"font-size: 16px; color: {self.campo_texto_cor}; font-family: Consolas;")
-        self.label_mensagem.setAlignment(Qt.AlignCenter)
-        self.label_mensagem.setFocusPolicy(Qt.NoFocus) 
-        self.label_mensagem.hide()  
-        layout.addWidget(self.label_mensagem)
-
-        
-        self.central_widget.setLayout(layout)
-
-        # Estilizar o fundo da janela
-        self.setStyleSheet(f"background-color: {self.fundo_cor}; font-family: Consolas;")
-
-    def toggle_senha_visivel(self):
-        if self.campo_senha.echoMode() == QLineEdit.Password:
-            self.campo_senha.setEchoMode(QLineEdit.Normal)
-            self.botao_olho.setText("❌")
-        else:
-            self.campo_senha.setEchoMode(QLineEdit.Password)
-            self.botao_olho.setText("👁️")
-
-    def fazer_login(self):
-        usuario_campo = self.campo_usuario.text().strip()
-        senha_campo = self.campo_senha.text().strip()
-
-        try:
-            user_ref = ref.child(f"Usuario/{usuario_campo}")
-            user_data = user_ref.get()
-
-            # Exibe a mensagem e altera a cor
-            self.label_mensagem.show()
-
-            if user_data:
-                senha_servidor = user_data.get("Senha")
-                if senha_servidor == senha_campo:
-                    QApplication.processEvents() 
-                    self.label_mensagem.setText("")
-                    self.label_mensagem.setText("Usuário logado")
-                    self.label_mensagem.setStyleSheet("color: green; font-size: 16px; font-family: Consolas;")
-
-                    QApplication.processEvents() 
-                    QTimer.singleShot(300, self.close)
-
-                   
-                    ui.campo_usuario.setText(f"{usuario_campo}")
-                    ui.campo_senha_usuario.setText(f"{senha_servidor}")
-
-                    janela.show()
-
-                else:
-                    self.label_mensagem.setText("")
-                    self.label_mensagem.setText("Senha inválida")
-                    self.label_mensagem.setStyleSheet("color: red; font-size: 16px; font-family: Consolas;")
-                    QApplication.processEvents() 
-            else:
-                self.label_mensagem.setText("")
-                self.label_mensagem.setText("Usuário não encontrado")
-                self.label_mensagem.setStyleSheet("color: red; font-size: 16px; font-family: Consolas;")
-                QApplication.processEvents() 
-
-        except Exception as e:
-            self.label_mensagem.setText("")
-            self.label_mensagem.setText("Erro de conexão com o servidor")
-            self.label_mensagem.setStyleSheet("color: red; font-size: 16px; font-family: Consolas;")
-            print("Erro ao conectar ao Firebase:", e)
-
-
-
 class JanelaOculta:
     def __init__(self, pai):
         self.pai = pai
@@ -3174,7 +3044,7 @@ janela.move(x, y)
 janela.setWindowTitle("Auxiliar")
 janela.setFixedSize(151, 53)
 
-janelaLogin = LoginWindow()        
+janelaLogin = LoginWindow(janela,ui)        
 janelaLogin.show()
 
 
